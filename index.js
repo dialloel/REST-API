26828d3c69011f9afbd28d018a2f4c8aaacf0be4
dialloel <dialloelhadji90@hotmail.fr>
2023-03-17 01:37:32 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express"); 
+const app = express();
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+const userRoute = require("./routes/user")
+const authRoute = require("./routes/auth")
+const productRoute = require("./routes/product")
+const cartRoute = require("./routes/cart")
+const orderRoute = require("./routes/order")
+
+
+
+dotenv.config();
+
+
+
+mongoose
+.connect(
+    process.env.MONGO_URL
+    )
+    .then(()=>console.log("DB Connection Successfull"))
+    .catch((err)=> {
+        console.log(err)
+    })
+
+    // Create a rest API
+
+    // app.get("/api/test", () => {
+    //     console.log("test is successfull")
+    // })
+
+app.use(express.json());
+app.use("/api/auth",authRoute);
+app.use("/api/users",userRoute);
+app.use("/api/products",productRoute);
+app.use("/api/carts",cartRoute);
+app.use("/api/orders",orderRoute);
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log ("Backed server is running!");
+})
